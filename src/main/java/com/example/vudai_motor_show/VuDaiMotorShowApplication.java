30c759fda0d6e38b29e17f6af4bdbb269131578e
Pham Vu Dai <dai_pv@EVN22013-DaiPV.local>
2022-06-08 13:22:45 +0700

    creat project and config

---
+++ src/main/java/com/example/vudai_motor_show/VuDaiMotorShowApplication.java
@@ -0,0 +1,13 @@
+package com.example.vudai_motor_show;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuDaiMotorShowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VuDaiMotorShowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vudai_motor_show/VuDaiMotorShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vudai_motor_show;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuDaiMotorShowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
